689d027bda220a845649ca04d69e791629448bba
SalmaZiady <salma.elziady@yahoo.com>
2026-02-07 22:02:59 +0200

    Initial commit: React career connect setup

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const name = "John";
+const x = 10;
+const y = 20;
+const names = ['Brad', 'Mary', 'Joe', 'Sara']
+const loggedIn = false;
+
+const App = () => {
+  return (
+    <>
+      {" "}
+      <div className="text-5xl">App</div>
+      <p> Hello {name} </p>
+      <p>
+        The sum of {x} and {y} is {x + y}
+      </p>
+      <ul>
+        {
+          names.map((name, index)=> (
+            <li key= { index }>
+              {name}
+            </li>
+          ))
+        }
+      </ul>
+      {
+        loggedIn
+           &&
+        <h1>Hello Guest </h1>
+      }
+    </>
+    
+  );
+};
+
+export default App;
