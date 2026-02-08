098b64a0eee65750d59ec441e4e5670f3c1eb8bd
SalmaZiady <salma.elziady@yahoo.com>
2026-02-08 15:55:29 +0200

    React Router with pages folder created

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<HomePage />} />)
+)
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards></HomeCards>
-      <JobListings />
-      <ViewAllJobs />
-    </>
+     <RouterProvider router={router}> </RouterProvider>
   );
 };
 
