f449282feebfb2afc73375c5aafeb94718da40a8
SalmaZiady <salma.elziady@yahoo.com>
2026-02-08 22:20:56 +0200

    Used React Link in Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
+
 import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<HomePage />} />)
-)
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout/>}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+    </Route>
+  )
+);
 
-const App = () => {
-  return (
-     <RouterProvider router={router}> </RouterProvider>
-  );
-};
+
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
